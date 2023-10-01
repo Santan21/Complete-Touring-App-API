336b2610baaff5a06af3ed8482a28e47262b199e
Santan21 <lesliegyamfi02@gmail.com>
2023-10-01 23:44:26 +0000

    tried out other HTTP methods

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-   res.status(200).send('This is the server');
+app.get("/", (req, res) => {
+  res
+  .status(200)
+  .json({ message:"This is the server", app: "Touring App" });
 });
+
+app.post('/', (req, res) => {
+ res.send("We'll post to this endpoint")
+})
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log('App running on port ${port}.... ');
+  console.log("App running on port ${port}.... ");
 });
