90a91433d22c1e61dde3f4729b81aaf56251fa84
Santan21 <lesliegyamfi02@gmail.com>
2023-10-08 17:27:13 +0000

    still refactoring routes

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ const getAllTours = (req, res) => {
 app.get("/api/v1/tours/:id", getAllTours);
 
 
-app.post("/api/v1/tours", (req, res) => {
+
+const CreateTours = (req, res) => {
   //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -48,7 +49,10 @@ app.post("/api/v1/tours", (req, res) => {
       },
     });
   });
-});
+}
+app.post("/api/v1/tours", CreateTours);
+
+
 
 app.patch('/api/v1/tours/:id', (req,res) => {
   if ( req.params.id * 1 > tours.length) {
@@ -64,6 +68,8 @@ app.patch('/api/v1/tours/:id', (req,res) => {
     }
   });
 });
+
+
 app.delete('/api/v1/tours/:id', (req,res) => {
   if ( req.params.id * 1 > tours.length) {
     return res.status(404).json({
