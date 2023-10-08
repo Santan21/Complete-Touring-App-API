12eff43daad7f75c26ffa684acebce7e5a02f9bf
Santan21 <lesliegyamfi02@gmail.com>
2023-10-08 13:48:14 +0000

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
-const filePath ='/Users/mac/Desktop/Backend Engineering/Touring App API/assets/data/tours-simple.json';
+const filePath =
+  "/Users/mac/Desktop/Backend Engineering/Touring App API/assets/data/tours-simple.json";
 app.use(express.json());
 
 //app.get("/", (req, res) => {
@@ -13,35 +14,51 @@ app.use(express.json());
 //app.put("/update", (req, res) => {
 //  res.send("We update data here baby!");
 //});
-const tours = JSON.parse(
-  fs.readFileSync(filePath, 'utf-8')
-);
-app.get('/api/v1/tours', (req, res) => {
+const tours = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "Success",
     data: {
       results: tours.length,
-    tours
-    }   
+      tours,
+    },
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: "Success",
+    data: { tour },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
   //console.log(req.body);
-  const newId = tours[tours.length -1].id + 1;
+  const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
-  tours.push(newTour)
-  fs.writeFile(filePath , JSON.stringify(tours), err =>{
-  res.status(201).json({
-    status: "Success",
-    data: {
-      tour: newTour
-    }
-  })
-  })
-  res.send('Done!');
-})
+  tours.push(newTour);
+
+  fs.writeFile(filePath, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: "Success",
+      data: {
+        tour: newTour,
+      },
+    });
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
