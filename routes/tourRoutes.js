c0ce99ec85effa60414e55b66fd914c41026b2cc
Santan21 <lesliegyamfi02@gmail.com>
2023-11-06 16:09:30 +0000

    moved route handlers from root file to resp. route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,75 @@
-const tourRouter = express.Router();
 const express = require('express')
+
+const getAllTours = (req, res) => {
+    res.status(200).json({
+      status: "Success",
+      requestedAt: req.requestTime,
+      data: {
+        results: tours.length,
+        tours,
+      },
+    });
+  };
+  
+  const getTours = (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+    //if (id > tours.length) {
+    if (!tour) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invalid ID",
+      });
+    }
+    res.status(200).json({
+      status: "Success",
+      data: { tour },
+    });
+  };
+  const createTours = (req, res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(filePath, JSON.stringify(tours), (err) => {
+      res.status(201).json({
+        status: "Success",
+        data: {
+          tour: newTour,
+        },
+      });
+    });
+  };
+  const updateTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invalid ID",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour: "<Updated Tour>",
+      },
+    });
+  }
+  const deleteTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invalid ID",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: null,
+    });
+  }
+
+const tourRouter = express.Router();
+
 tourRouter
 .route('/')
 .get(getAllTours)
@@ -10,3 +80,5 @@ tourRouter
 .get(getTours)
 .patch(updateTour)
 .delete(deleteTour);
+
+module.exports = router;
