6ff05b791c6a5b1dec63cb3c3f40ba84c23b1181
Santan21 <lesliegyamfi02@gmail.com>
2023-11-17 00:27:57 +0000

    moved server to server.js file

--- app.js
+++ app.js
@@ -20,8 +20,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-//SERVER
-const port = 3001;
-app.listen(port, () => {
-  console.log("App running on port ${port}.... ");
-});
+module.exports = app;
