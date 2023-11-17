8096c708b0caf2cb9bc56e97c1e4875709143453
Santan21 <lesliegyamfi02@gmail.com>
2023-11-17 14:27:54 +0000

    param middleware - done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,13 +4,13 @@ const filePath = "/Users/mac/Desktop/BE/Touring App/assets/data/tours-simple.jso
 const tours = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
 exports.checkID = (req, res, next, val) => {
+  console.log('Tour id is ${val}');
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
       message: "Invalid ID",
     });
   }
-
   next();
 };
 
@@ -32,12 +32,6 @@ exports.getAllTours = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find((el) => el.id === id);
     //if (id > tours.length) {
-    if (!tour) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-      });
-    }
     res.status(200).json({
       status: "Success",
       data: { tour },
@@ -58,12 +52,6 @@ exports.getAllTours = (req, res) => {
     });
   };
   exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-      });
-    }
     res.status(200).json({
       status: "success",
       data: {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,10 +4,7 @@ const filePath = "/Users/mac/Desktop/BE/Touring App/assets/data/tours-simple.jso
 const tourController = require('./../controllers/tourController');
 const tourRouter = express.Router();
 
-tourRouter.param('id', (req, res, next, val) => {
-    console.log('Tour id is ${val}');
-    next();
-} );
+tourRouter.param('id', tourController.checkID);
 
 tourRouter
 .route('/')
