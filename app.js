f5a8fa700ae6e3f5a2ee703bcca77205cc9c5cd0
Santan21 <lesliegyamfi02@gmail.com>
2023-12-07 18:00:11 +0000

    setting up production and dev envs

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const app = express();
 
 if  (process.env.NODE_ENV === 'development') { 
   app.use(morgan('dev'));
- }
-
+ };
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -22,7 +21,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date(). toISOString();
   next();
 });
-
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
