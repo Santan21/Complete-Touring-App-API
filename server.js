2ae8b279be834f7a73ea43b91881e17431971e07
Santan21 <lesliegyamfi02@gmail.com>
2023-12-09 20:54:26 +0000

    connectinng the database to the express app

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = require('./app');
 
 dotenv.config({path: './config.env'});
 
+const DB = process.env.Database.replace(
+  '<PASSWORD>', 
+  process.env.DATABASE_PASSWORD);
+
+mongoose.connect(); 
+
+
 //SERVER
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
