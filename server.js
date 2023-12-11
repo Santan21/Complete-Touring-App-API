3f961f88824d048f1e3b471a2ba902e9f3e33b6a
Santan21 <lesliegyamfi02@gmail.com>
2023-12-11 22:18:08 +0000

    creating docs and testing models

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv =  require('dotenv');
 const app = require('./app');
 
 dotenv.config({path: './config.env'});
-
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>', 
   process.env.DATABASE_PASSWORD
@@ -17,18 +16,35 @@ mongoose
 })
   .then(() =>
     console.log('DB Connection Successful'));
+    
+    const tourSchema = new mongoose.Schema({
+      name: {
+        type: 'string',
+        required: [true, 'A tour must have a name'],
+        unique: true,
+      },
+      rating: {
+        type: Number,
+        default: 4.5
+      },
+      price: { 
+        type: 'number',
+        required: [true, 'A tour must have a price']
+      }
+    });
+    const Tour = mongoose.model('Tour', tourSchema);
 
-  const tourSchema = new mongoose.Schema({
-    name: {
-      type: 'string',
-      required: true,
-    },
-    rating: Number,
-    price: {
-      type: 'number',
-      required: true
-    }
-  });
+    const testTour = new Tour ({
+      name: 'Fuji Explorer',
+      rating: '4.1',
+      price: 217
+    });
+
+    testTour
+    .save()
+    .then(doc => {
+      console.log(doc);
+    }) 
 
 //SERVER
 const port = process.env.PORT || 3001;
